0afdd265ba2b8ff77c7c7b80ec7475808b37c913
Austin Isaac Onwuka <austinisaaconwuka@gmail.com>
2026-08-03 19:56:09 +0100

    connect MongoDB Atlars

---
+++ server/models/Delegate.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+
+const delegateSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    designation: {
+      type: String,
+      required: true,
+    },
+
+    country: {
+      type: String,
+      default: "",
+    },
+
+    profileImage: {
+      type: String,
+      default: "",
+    },
+
+    bio: {
+      type: String,
+      default: "",
+    },
+
+    summit: {
+      type: String,
+      required: true,
+    },
+
+    summitYear: {
+      type: Number,
+      required: true,
+    },
+
+    summitGroup: {
+      type: String,
+      default: "",
+    },
+
+    certificates: [
+      {
+        title: String,
+        certificateNumber: String,
+        issuedDate: Date,
+        image: String,
+      },
+    ],
+
+    awards: [
+      {
+        title: String,
+        image: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Delegate", delegateSchema);
